f3d8cd0a6fd53a7118c9a5ce1baea693a16ead69
Adi Shrestha <adishrestha2005@gmail.com>
2026-07-11 16:58:57 +0545

    mongodb connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,21 @@
 import dotenv from "dotenv";
-import app from "./app";
-
 dotenv.config();
+
+import app from "./app";
+import connectDB from "./config/database";
 
 const PORT = process.env.PORT || 5050;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+startServer();
